c46547ebf62db533771399bae877a6112ce55a00
Deva <deva.thevaranchan@gmai.com>
2024-01-30 14:58:08 +0100

    API

--- api.py
+++ api.py
@@ -1,58 +1,18 @@
 from flask import Flask, render_template, request, jsonify, url_for
 import requests
+import random
+
 
 
 ecolabs = {
 
-        "ecolab_2": {
-            'Cellule_1': {
-                "name": "E2C1",
-                "fins": {
-                    "ip": "192.168.0.21",
-                    "port": 9600,
-                    "dst_net_addr": 2,
-                    "dst_node_num": 2,
-                    "dst_unit_addr": 0
-                },
-                "params": {
-                    "temperature_reprise": 10,
-                    "temperature_eau_pluie": 15,
-                    "temperature_consigne": 0
-                }
-            },
-            'Cellule_2':{
-                    "name": "E2C2",
-                    "fins": {
-                        "ip": "192.168.0.2",
-                        "port": 9600,
-                        "dst_net_addr": 1,
-                        "dst_node_num": 1,
-                        "dst_unit_addr": 0
-                    },
-                    "params": {
-                        "temperature_reprise": 11,
-                        "temperature_eau_pluie": 16,
-                        "temperature_consigne": 0
-                    }
-            },
-            'Cellule_3':{
-                    "name": "E2C3",
-                    "fins": {
-                        "ip": "192.168.0.3",
-                        "port": 9600,
-                        "dst_net_addr": 1,
-                        "dst_node_num": 1,
-                        "dst_unit_addr": 0
-                    },
-                    "params": {
-                        "temperature_reprise": 12,
-                        "temperature_eau_pluie": 17,
-                        "temperature_consigne": 0
-                    }
-            }
+             "params": {
+                 "temperature_reprise": round(random.uniform(-10, 45), 0),
+                 "temperature_eau_pluie": round(random.uniform(-10, 45), 0),
+                "temperature_consigne": 0
+             }
+           
         }
-
-    }
 
 # country = input("lr nom de la vile :")
 
@@ -73,7 +33,7 @@ class Api():
         self.ecolab_5 = "http://10.119.50.100:8080/"
         self.ecolab_6 = "http://10.119.60.100:8080/"
 
-    def get_ecolab_1(self):
+    def get_ecolab_1(self,cell):
         try:
             json_data = requests.get(self.ecolab_1).json()
             # Faire quelque chose avec json_data
@@ -82,17 +42,94 @@ class Api():
             json_data = None
 
         if json_data is None:
+            return ecolabs["params"]
+        else:
+            return json_data['ecolab_1'][cell]['params']
+
+    def get_ecolab_2(self,cell):
+        try:
+            json_data = requests.get(self.ecolab_2).json()
+            # Faire quelque chose avec json_data
+        except Exception as e:
+            print(f"Problème lors de la récupération des données : {e}")
+            json_data = None
+
+        if json_data is None:
+            return ecolabs["params"]
+        else:
+            return json_data['ecolab_2'][cell]['params']
+
+    def get_ecolab_3(self,cell):
+        try:
+            json_data = requests.get(self.ecolab_3).json()
+            # Faire quelque chose avec json_data
+        except Exception as e:
+            print(f"Problème lors de la récupération des données : {e}")
+            json_data = None
+
+        if json_data is None:
+            return ecolabs["params"]
+        else:
+            return json_data['ecolab_3'][cell]['params']
+        
+    def get_ecolab_4(self,cell):
+        try:
+            json_data = requests.get(self.ecolab_4).json()
+            # Faire quelque chose avec json_data
+        except Exception as e:
+            print(f"Problème lors de la récupération des données : {e}")
+            json_data = None
+
+        if json_data is None:
             print("Problème lors de la récupération des données.")
         else:
-            print("Données récupérées avec succès.")
-        
+            return json_data['ecolab_4'][cell]['params']
     
-    
-    
+    def get_ecolab_5(self,cell):
+        try:
+            json_data = requests.get(self.ecolab_5).json()
+            # Faire quelque chose avec json_data
+        except Exception as e:
+            print(f"Problème lors de la récupération des données : {e}")
+            json_data = None
 
-api = Api()
-test = api.get_ecolab_1()
-   
+        if json_data is None:
+            print("Problème lors de la récupération des données.")
+        else:
+            return json_data['ecolab_5'][cell]['params']
+    
+    def get_ecolab_6(self,cell):
+        try:
+            json_data = requests.get(self.ecolab_6).json()
+            # Faire quelque chose avec json_data
+        except Exception as e:
+            print(f"Problème lors de la récupération des données : {e}")
+            json_data = None
+
+        if json_data is None:
+            print("Problème lors de la récupération des données.")
+        else:
+            return json_data['ecolab_6'][cell]['params']
+
+    def get_ecolab(self,numecolab,cell):
+        cellule =f"Cellule_{cell}"
+        if numecolab == 1:
+            return self.get_ecolab_1(cellule) 
+        if numecolab == 2:
+            return self.get_ecolab_2(cellule) 
+        if numecolab == 3:
+            return self.get_ecolab_3(cellule) 
+        if numecolab == 4:
+            return self.get_ecolab_4(cellule) 
+        if numecolab == 5:
+            return self.get_ecolab_5(cellule) 
+        if numecolab == 6:
+            return self.get_ecolab_6(cellule) 
+
+
+API = Api()
+# test = API.get_ecolab(2,"2")
+# print(round(test["temperature_reprise"]))
     
 
 
--- bot.py
+++ bot.py
@@ -6,11 +6,91 @@ pip install discord.py
 
 import discord
 from discord.ext import commands
+from discord import Button, ButtonStyle
+from api import API
 
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='Octopus ', intents=intents)
+bot = commands.Bot(command_prefix='', intents=intents)
+
+class Params(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+        
+
+    @discord.ui.button(label = "Temperature Reprise", style=discord.ButtonStyle.grey)
+    async def menu1(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = "La température de reprise a été sélectionnée."
+        await interaction.response.send_message("Température Reprise")
+        
+    @discord.ui.button(label = "Temperature Consigne", style=discord.ButtonStyle.grey)
+    async def menu2(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = "La température de reprise a été sélectionnée."
+        await interaction.response.send_message("Température Consigne")  
+
+    @discord.ui.button(label = "Temperature Eau Pluie", style=discord.ButtonStyle.grey)
+    async def menu3(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = "La température de reprise a été sélectionnée."
+        await interaction.response.send_message("Température Eau Pluie")
+
+class Ecolabs(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        
+    @discord.ui.button(label = "ECOLAB 1", style=discord.ButtonStyle.green)
+    async def menu1(self, interaction: discord.Interaction, button: discord.ui.Button):
+        
+        await interaction.response.send_message("ECOLAB 1")  
+
+    @discord.ui.button(label = "ECOLAB 2", style=discord.ButtonStyle.green)
+    async def menu2(self, interaction: discord.Interaction, button: discord.ui.Button):
+        
+        await interaction.response.send_message("ECOLAB 2")  
+
+    @discord.ui.button(label = "ECOLAB 3", style=discord.ButtonStyle.green)
+    async def menu3(self, interaction: discord.Interaction, button: discord.ui.Button):
+        
+        await interaction.response.send_message("ECOLAB 3")  
+
+    @discord.ui.button(label = "ECOLAB 4", style=discord.ButtonStyle.green)
+    async def menu4(self, interaction: discord.Interaction, button: discord.ui.Button):
+        
+        await interaction.response.send_message("ECOLAB 4")  
+
+    @discord.ui.button(label = "ECOLAB 5", style=discord.ButtonStyle.green)
+    async def menu5(self, interaction: discord.Interaction, button: discord.ui.Button):
+        
+        await interaction.response.send_message("ECOLAB 5")  
+
+    @discord.ui.button(label = "ECOLAB 6", style=discord.ButtonStyle.green)
+    async def menu6(self, interaction: discord.Interaction, button: discord.ui.Button):
+        
+        await interaction.response.send_message("ECOLAB 6")  
+
+
+class Cellules(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        
+    @discord.ui.button(label = "Cellule 1", style=discord.ButtonStyle.green)
+    async def menu1(self, interaction: discord.Interaction, button: discord.ui.Button):
+        
+        await interaction.response.send_message("Cellule 1")  
+
+    @discord.ui.button(label = "Cellule 2", style=discord.ButtonStyle.green)
+    async def menu2(self, interaction: discord.Interaction, button: discord.ui.Button):
+        
+        await interaction.response.send_message("Cellule 2")  
+
+    @discord.ui.button(label = "Cellule 3", style=discord.ButtonStyle.green)
+    async def menu3(self, interaction: discord.Interaction, button: discord.ui.Button):
+        
+        await interaction.response.send_message("Cellule 3")  
+
+   
+
 
 
 def param():
@@ -27,6 +107,10 @@ async def on_member_join(member):
     await channel.send(f'Welcome {member}')
 
 
+
+
+
+
 @bot.command()
 async def clear(ctx, nombre: int = 1):
     async for message in ctx.channel.history(limit=nombre +1):
@@ -38,7 +122,7 @@ async def clearAll(ctx):
         await message.delete()
 
 @bot.command()
-async def coucou(ctx):
+async def Bonjour(ctx):
     user = ctx.author.name
     user_without_last_4 = user[:-4]
     await ctx.send(f"Bonjour {user_without_last_4}")
@@ -70,24 +154,73 @@ async def unban(ctx, user , *reason):
    
 @bot.command()
 async def Ecolab(ctx, numEcolab :int = None, cellule:str = None ,numCell :int = None):
-    if numEcolab is None:
-         await ctx.send("quel Ecolab voulez vous voir?")
-         ecolab  = await bot.wait_for("message",timeout=60)
-         eco = ecolab.content.split(" ")
-         for ecolab in eco:
-             if ecolab.isdigit():
-                numEcolab = ecolab
-
+   
+    if numEcolab is None :
          
-    await ctx.send("quel cellue voulez vous voir?")
-    cellule = await bot.wait_for("message",timeout=60)
-    cell = cellule.content.split(" ")
-    for cellue in cell:
-        if cellue.isdigit():
-            numCell = cellue     
-    await ctx.send("Quel parametre de la cellule voulez vous consulter? ")
-    parametre = await bot.wait_for("message",timeout=60 )
-    await ctx.send(f"La {parametre.content} de l'Ecolab {numEcolab}, cellule {numCell} est 20.")
+         while (True):
+
+            await ctx.reply("Quel Ecolab voulez-vous voir?")
+            eco = Ecolabs()
+            await ctx.send(view = eco)
+            ecolab  = await bot.wait_for("message",timeout=60)
+            eco = ecolab.content.split(" ")
+            for ecolab in eco:
+                if ecolab.isdigit():
+                    numEcolab = int(ecolab)
+            if numEcolab < 1 or numEcolab >6 :
+                await ctx.reply(f"Ecolab {numEcolab} n'existe pas. Veuillez choisir un Ecolab valide!.")
+
+            if numEcolab >=1 and numEcolab <=6:
+                break
+            
+    while (True):     
+        await ctx.reply(f"Quel Cellue de l'Ecolab {numEcolab} voulez vous voir ?")
+        cel = Cellules()
+        await ctx.send(view=cel)
+        cellule = await bot.wait_for("message",timeout=60)
+        cell = cellule.content.split(" ")
+        for cellue in cell:
+            if cellue.isdigit():
+                numCell = int(cellue)
+        if numCell < 1 or numCell >3 :
+            await ctx.reply(f"La Cellule {numCell} n'existe pas. Veuillez choisir une cellule valide!.")
+
+        if numCell >=1 and numCell <=3:
+            break 
+        
+    api = API.get_ecolab(numEcolab,numCell)
+    while (True):
+        await ctx.reply(f"Quel paramètre de la Cellule {numCell}, Ecolab {numEcolab} voulez vous consulter? ")
+        param = Params()
+        await ctx.send(view=param)
+        parametre = await bot.wait_for("message",timeout=60 )
+        temperature = parametre.content.lower()
+        temperature_reprise= "température reprise"
+        temperature_eau_pluie = "température eau pluie"
+        temperature_consigne=  "température consigne"
+
+
+        if temperature == temperature_reprise :
+            temperature_reprise= "temperature_reprise"
+            temperature = temperature_reprise
+            break
+        if temperature == temperature_consigne :
+            temperature_consigne=  "temperature_consigne"
+            temperature=temperature_consigne
+            break
+        if temperature == temperature_eau_pluie:
+            temperature_eau_pluie = "temperature_eau_pluie"
+            temperature = temperature_eau_pluie
+            break
+        else:
+            await ctx.reply("Le paramètre que vous avez saisi est incorrect. Veuillez choisir le paramètre correct.")
+    
+    
+    await ctx.reply(f"La {parametre.content} de l'Ecolab {numEcolab}, cellule {numCell} est {round(api[temperature])}.")
+
+
+            
 
 
 
+bot.run("MTIwMDQ0OTQ4MzAyNTE2NjQ0Ng.GxX85c.Dd2U7kN6iKs5WhFyqlMQ9BKg4aFb8Qua32PQ-k")
